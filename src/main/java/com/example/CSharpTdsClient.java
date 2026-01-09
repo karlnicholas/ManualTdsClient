99ae022dc3f126034d72307836a9740b572770da
Karl Nicholas <karl.nicholas@outlook.com>
2026-01-09 08:08:00 -0500

    Updates.

--- src/main/java/com/example/CSharpTdsClient.java
+++ src/main/java/com/example/CSharpTdsClient.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import org.tdslib.javatdslib.QueryResponse;
+import org.tdslib.javatdslib.ResultSet;
 import org.tdslib.javatdslib.TdsClient;
+
+import java.util.List;
 
 public class CSharpTdsClient {
     public static void main(String[] args) throws Exception {
@@ -12,7 +16,14 @@ public class CSharpTdsClient {
         int port = 1433;
         try ( TdsClient client = new TdsClient(hostname, port) ) {
             client.connect("localhost", "reactnonreact", "reactnonreact", "reactnonreact", "app", "MyServerName", "us_english");
-            client.query("select @@version");
+            QueryResponse queryResponse = client.query("select @@version");
+            for(ResultSet resultSet: queryResponse.getResultSets() ) {
+                for ( List<byte[]> byteList: resultSet.getRawRows() ) {
+                    for( byte[] row: byteList) {
+                        System.out.println(new String(row));
+                    }
+                }
+            }
         }
 // If no error token was received, and SQL server did not close the connection, then the connection to the server is now established and the user is logged in.
     }
