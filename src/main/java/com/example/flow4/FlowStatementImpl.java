80065eac4344d3c29e76f7b46b0190948fb0ac70
Karl Nicholas <karl.nicholas@outlook.com>
2026-01-29 15:55:15 -0500

    Updates for rpc

--- src/main/java/com/example/flow4/FlowStatementImpl.java
+++ src/main/java/com/example/flow4/FlowStatementImpl.java
@@ -4,9 +4,11 @@ import java.util.concurrent.Flow;
 
 public class FlowStatementImpl implements FlowStatement {
   private final FlowRowPublisher source;
+  private boolean completed;
 
   public FlowStatementImpl(FlowRowPublisher source) {
     this.source = source;
+    this.completed = false;
   }
 
   @Override
@@ -16,7 +18,9 @@ public class FlowStatementImpl implements FlowStatement {
       subscriber.onSubscribe(new Flow.Subscription() {
         @Override
         public void request(long n) {
-          if (n > 0) {
+          // Use a boolean guard to ensure we run only once
+          if (!completed && n > 0) {
+            completed = true;
             subscriber.onNext(new FlowResultImpl(source));
             subscriber.onComplete();
           }
